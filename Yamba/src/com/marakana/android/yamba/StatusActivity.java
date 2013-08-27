df17f425a3cd0b647d0cc3be51342d1077b6eb38
G. Blake Meike <blake.meike@gmail.com>
2013-08-27 17:52:49 -0400

    Async Task posting

--- Yamba/src/com/marakana/android/yamba/StatusActivity.java
+++ Yamba/src/com/marakana/android/yamba/StatusActivity.java
@@ -1,54 +1,140 @@
 package com.marakana.android.yamba;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.content.res.Resources;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class StatusActivity extends Activity {
+import com.marakana.android.yamba.clientlib.YambaClient;
+import com.marakana.android.yamba.clientlib.YambaClientException;
+
+public class StatusActivity extends Activity  {
     private static final String TAG = "STATUS";
 
 
+    static class Poster extends AsyncTask<String, Void, Integer> {
+        private final Context ctxt;
+
+        public Poster(Context ctxt) { this.ctxt = ctxt; }
+
+        // Runs on daemon thread
+        @Override
+        protected Integer doInBackground(String... status) {
+            YambaClient client = new YambaClient(
+                    "student",
+                    "password",
+                    "http://yamba.marakana.com/api");
+
+            int ret = R.string.post_failed;
+            try {
+                client.postStatus(status[0]);
+                ret = R.string.post_succeeded;
+            }
+            catch (YambaClientException e) {
+                Log.e(TAG, "Post failed", e);
+            }
+
+            return Integer.valueOf(ret);
+        }
+
+        // Runs on UI thread
+        @Override
+        protected void onPostExecute(Integer ret) {
+            Toast.makeText(ctxt, ret.intValue(), Toast.LENGTH_LONG).show();
+            poster = null;
+        }
+    }
+
+    static Poster poster;
+
+
+    private int maxStatusLen;
+    private int warnMax;
+    private int errMax;
+    private int okColor;
+    private int warnColor;
+    private int errColor;
+    private EditText viewStatus;
+    private TextView viewCount;
+    private Button buttonSubmit;
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle state) {
+        super.onCreate(state);
         if (BuildConfig.DEBUG) { Log.d(TAG, "called onCreate: " + this); }
+
+        Resources rez = getResources();
+        maxStatusLen = rez.getInteger(R.integer.status_max_len);
+        warnMax = rez.getInteger(R.integer.warn_max);
+        errMax = rez.getInteger(R.integer.err_max);
+
+        okColor = rez.getColor(R.color.status_ok);
+        warnColor = rez.getColor(R.color.status_warn);
+        errColor = rez.getColor(R.color.status_err);
+
         setContentView(R.layout.activity_status);
+
+        viewCount = (TextView) findViewById(R.id.status_count);
+
+        buttonSubmit = (Button) findViewById(R.id.status_submit);
+        buttonSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) { post(); }
+        });
+
+        viewStatus = (EditText) findViewById(R.id.status_status);
+        viewStatus.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) { updateCount(); }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) { }
+        } );
     }
 
-    @Override
-    protected void onDestroy() {
-        if (BuildConfig.DEBUG) { Log.d(TAG, "called onDestroy"); }
-        super.onDestroy();
+    void updateCount() {
+        int n = viewStatus.getText().length();
+
+        buttonSubmit.setEnabled(checkStatusLen(n));
+
+        n = maxStatusLen - n;
+
+        int c;
+        if (n > warnMax) { c = okColor; }
+        else if (n > errMax) { c = warnColor; }
+        else { c = errColor; }
+
+        viewCount.setText(String.valueOf(n));
+        viewCount.setTextColor(c);
     }
 
-    @Override
-    protected void onRestart() {
-        if (BuildConfig.DEBUG) { Log.d(TAG, "called onRestart"); }
-        super.onRestart();
+    void post() {
+        if (null != poster) { return; }
+
+        String status = viewStatus.getText().toString();
+        if (BuildConfig.DEBUG) { Log.d(TAG, "Posting: " + status); }
+
+        if (!checkStatusLen(status.length())) { return; }
+
+        viewStatus.setText("");
+
+        poster = new Poster(getApplicationContext());
+        poster.execute(status.toString());
     }
 
-    @Override
-    protected void onPause() {
-        if (BuildConfig.DEBUG) { Log.d(TAG, "called onPause"); }
-        super.onPause();
+    private boolean checkStatusLen(int n) {
+        return (0 < n) && (maxStatusLen >= n);
     }
-
-    @Override
-    protected void onResume() {
-        if (BuildConfig.DEBUG) { Log.d(TAG, "called onResume"); }
-        super.onResume();
-    }
-
-    @Override
-    protected void onStart() {
-        if (BuildConfig.DEBUG) { Log.d(TAG, "called onStart"); }
-        super.onStart();
-    }
-
-    @Override
-    protected void onStop() {
-        if (BuildConfig.DEBUG) { Log.d(TAG, "called onStop"); }
-        super.onStop();
-    }
-
 }
